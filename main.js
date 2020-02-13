0eb4d2d1e3fdf3f674641045e375a99212f6560f
David Obodo <obododavid5@gmail.com>
2020-02-13 21:49:51 +0100

    displaying different tabs on click on their  headers

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 var hamburger = document.querySelector('.nav__hamburger');
 var cancel = document.querySelector('.cancel-icon');
 var mobile_nav = document.querySelector('.nav__links');
+var tab1title = document.querySelector('.tab-1-title');
+var tab2title = document.querySelector('.tab-2-title');
+var tab3title = document.querySelector('.tab-3-title');
+var tab1 = document.querySelector('#tab-1');
+var tab2 = document.querySelector('#tab-2');
+var tab3 = document.querySelector('#tab-3');
 var handleShowMobileNavbar = function () {
     mobile_nav.style.transform = 'translateX(0px)';
 };
@@ -9,3 +15,37 @@ var handleHideMobileNavbar = function () {
 };
 hamburger.addEventListener('click', handleShowMobileNavbar);
 cancel.addEventListener('click', handleHideMobileNavbar);
+var handleDisplayTabOne = function () {
+    tab2.style.display = 'none';
+    tab3.style.display = 'none';
+    if (window.screen.width <= 765) {
+        tab1.style.display = 'block';
+    }
+    else {
+        tab1.style.display = 'flex';
+    }
+};
+var handleDisplayTabTwo = function () {
+    tab1.style.display = 'none';
+    tab3.style.display = 'none';
+    if (window.screen.width <= 765) {
+        tab2.style.display = 'block';
+    }
+    else {
+        tab2.style.display = 'flex';
+    }
+};
+var handleDisplayTabThree = function () {
+    tab1.style.display = 'none';
+    tab2.style.display = 'none';
+    if (window.screen.width <= 765) {
+        tab3.style.display = 'block';
+    }
+    else {
+        tab3.style.display = 'flex';
+    }
+};
+handleDisplayTabOne();
+tab1title.addEventListener('click', handleDisplayTabOne);
+tab2title.addEventListener('click', handleDisplayTabTwo);
+tab3title.addEventListener('click', handleDisplayTabThree);
--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@ const hamburger = document.querySelector('.nav__hamburger');
 const cancel = document.querySelector('.cancel-icon');
 const mobile_nav: HTMLElement = document.querySelector('.nav__links');
 
+const tab1title = document.querySelector('.tab-1-title');
+const tab2title = document.querySelector('.tab-2-title');
+const tab3title = document.querySelector('.tab-3-title');
+
+const tab1: HTMLElement = document.querySelector('#tab-1');
+const tab2: HTMLElement = document.querySelector('#tab-2');
+const tab3: HTMLElement = document.querySelector('#tab-3');
+
 const handleShowMobileNavbar = () => {
     mobile_nav.style.transform = 'translateX(0px)';
 }
@@ -12,3 +20,46 @@ const handleHideMobileNavbar = () => {
 
 hamburger.addEventListener('click', handleShowMobileNavbar)
 cancel.addEventListener('click', handleHideMobileNavbar)
+
+
+const handleDisplayTabOne = () => {
+    tab2.style.display = 'none';
+    tab3.style.display = 'none';
+
+    if (window.screen.width <= 765) {
+        tab1.style.display = 'block';
+
+    } else {
+        tab1.style.display = 'flex';
+    }
+}
+
+const handleDisplayTabTwo = () => {
+    tab1.style.display = 'none';
+    tab3.style.display = 'none';
+
+    if (window.screen.width <= 765) {
+        tab2.style.display = 'block';
+
+    } else {
+        tab2.style.display = 'flex';
+    }
+}
+
+const handleDisplayTabThree = () => {
+    tab1.style.display = 'none';
+    tab2.style.display = 'none';
+
+    if (window.screen.width <= 765) {
+        tab3.style.display = 'block';
+
+    } else {
+        tab3.style.display = 'flex';
+    }
+}
+
+handleDisplayTabOne();
+
+tab1title.addEventListener('click', handleDisplayTabOne);
+tab2title.addEventListener('click', handleDisplayTabTwo);
+tab3title.addEventListener('click', handleDisplayTabThree);
