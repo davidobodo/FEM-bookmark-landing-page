faaf320930ea2ee2d7440cc26d0d27b0bacf4e94
David Obodo <obododavid5@gmail.com>
2020-02-17 15:23:56 +0100

    arrange main.ts file code properly (still includes a lot of repeated code)

--- main.ts
+++ main.ts
@@ -1,21 +1,11 @@
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//Navbar Logic(start)
+
 const hamburger = document.querySelector('.nav__hamburger');
 const cancel = document.querySelector('.cancel-icon');
 const mobile_nav: HTMLElement = document.querySelector('.nav__links');
-
-const tab1title = document.querySelector('.tab-1-title');
-const tab2title = document.querySelector('.tab-2-title');
-const tab3title = document.querySelector('.tab-3-title');
-
-const tab1titleUnderline: HTMLElement = document.querySelector('.tab-1-title span');
-const tab2titleUnderline: HTMLElement = document.querySelector('.tab-2-title span');
-const tab3titleUnderline: HTMLElement = document.querySelector('.tab-3-title span');
-
-
-const tab1: HTMLElement = document.querySelector('#tab-1');
-const tab2: HTMLElement = document.querySelector('#tab-2');
-const tab3: HTMLElement = document.querySelector('#tab-3');
-
-const faqs = document.querySelectorAll('.section__four__faqs__faq');
 
 const handleShowMobileNavbar = () => {
     mobile_nav.style.transform = 'translateX(0px)';
@@ -28,54 +18,78 @@ const handleHideMobileNavbar = () => {
 hamburger.addEventListener('click', handleShowMobileNavbar)
 cancel.addEventListener('click', handleHideMobileNavbar)
 
+//Navbar Logic(end)
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//Tabs Logic(start)
+const tab1title = document.querySelector('.tab-1-title');
+const tab2title = document.querySelector('.tab-2-title');
+const tab3title = document.querySelector('.tab-3-title');
+
+const tab1titleUnderline: HTMLElement = document.querySelector('.tab-1-title span');
+const tab2titleUnderline: HTMLElement = document.querySelector('.tab-2-title span');
+const tab3titleUnderline: HTMLElement = document.querySelector('.tab-3-title span');
+
+const tab1details: HTMLElement = document.querySelector('#tab-1');
+const tab2details: HTMLElement = document.querySelector('#tab-2');
+const tab3details: HTMLElement = document.querySelector('#tab-3');
 
 const handleDisplayTabOne = () => {
-    tab2.style.display = 'none';
-    tab3.style.display = 'none';
-
-
-
     if (window.screen.width <= 765) {
-        tab1.style.display = 'block';
-
+        tab1details.style.display = 'block';
     } else {
-        tab1.style.display = 'flex';
+        tab1details.style.display = 'flex';
     }
-
     tab1titleUnderline.style.display = 'inline';
+
+
+    tab2details.style.display = 'none';
     tab2titleUnderline.style.display = 'none';
+
+
+    tab3details.style.display = 'none';
     tab3titleUnderline.style.display = 'none';
 }
 
 const handleDisplayTabTwo = () => {
-    tab1.style.display = 'none';
-    tab3.style.display = 'none';
+    tab1details.style.display = 'none';
+    tab1titleUnderline.style.display = 'none';
+
 
     if (window.screen.width <= 765) {
-        tab2.style.display = 'block';
-
+        tab2details.style.display = 'block';
     } else {
-        tab2.style.display = 'flex';
+        tab2details.style.display = 'flex';
     }
-
-    tab1titleUnderline.style.display = 'none';
     tab2titleUnderline.style.display = 'inline';
+
+
+    tab3details.style.display = 'none';
     tab3titleUnderline.style.display = 'none';
 }
 
 const handleDisplayTabThree = () => {
-    tab1.style.display = 'none';
-    tab2.style.display = 'none';
+    tab1details.style.display = 'none';
+    tab1titleUnderline.style.display = 'none';
+
+
+    tab2details.style.display = 'none';
+    tab2titleUnderline.style.display = 'none';
+
 
     if (window.screen.width <= 765) {
-        tab3.style.display = 'block';
-
+        tab3details.style.display = 'block';
     } else {
-        tab3.style.display = 'flex';
+        tab3details.style.display = 'flex';
     }
-
-    tab1titleUnderline.style.display = 'none';
-    tab2titleUnderline.style.display = 'none';
     tab3titleUnderline.style.display = 'inline';
 }
 
@@ -84,6 +98,35 @@ handleDisplayTabOne();
 tab1title.addEventListener('click', handleDisplayTabOne);
 tab2title.addEventListener('click', handleDisplayTabTwo);
 tab3title.addEventListener('click', handleDisplayTabThree);
+
+// Tabs Logic(End)
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+// FAQ Logic (start)
+
+const faqOne = document.querySelector('#faq-one');
+const faqTwo = document.querySelector('#faq-two');
+const faqThree = document.querySelector('#faq-three');
+const faqFour = document.querySelector('#faq-four');
+
+const faqOneDetails: HTMLElement = document.querySelector('#faq-one .section__four__faqs__faq__details');
+const faqTwoDetails: HTMLElement = document.querySelector('#faq-two .section__four__faqs__faq__details');
+const faqThreeDetails: HTMLElement = document.querySelector('#faq-three .section__four__faqs__faq__details');
+const faqFourDetails: HTMLElement = document.querySelector('#faq-four .section__four__faqs__faq__details');
+
+const faqOneDirection: HTMLElement = document.querySelector('#faq-one .dropdown-direction');
+const faqTwoDirection: HTMLElement = document.querySelector('#faq-two .dropdown-direction');
+const faqThreeDirection: HTMLElement = document.querySelector('#faq-three .dropdown-direction');
+const faqFourDirection: HTMLElement = document.querySelector('#faq-four .dropdown-direction');
+
 
 const show = (elem) => {
     elem.style.display = 'block';
@@ -146,33 +189,28 @@ const handleDisplayFaqFour = (e) => {
 
 }
 
-const faqOne = document.querySelector('#faq-one');
-const faqTwo = document.querySelector('#faq-two');
-const faqThree = document.querySelector('#faq-three');
-const faqFour = document.querySelector('#faq-four');
-
-
-const faqOneDetails: HTMLElement = document.querySelector('#faq-one .section__four__faqs__faq__details');
-const faqTwoDetails: HTMLElement = document.querySelector('#faq-two .section__four__faqs__faq__details');
-const faqThreeDetails: HTMLElement = document.querySelector('#faq-three .section__four__faqs__faq__details');
-const faqFourDetails: HTMLElement = document.querySelector('#faq-four .section__four__faqs__faq__details');
-
-const faqOneDirection: HTMLElement = document.querySelector('#faq-one .dropdown-direction');
-const faqTwoDirection: HTMLElement = document.querySelector('#faq-two .dropdown-direction');
-const faqThreeDirection: HTMLElement = document.querySelector('#faq-three .dropdown-direction');
-const faqFourDirection: HTMLElement = document.querySelector('#faq-four .dropdown-direction');
-
 
 faqOne.addEventListener('click', handleDisplayFaqOne)
 faqTwo.addEventListener('click', handleDisplayFaqTwo)
 faqThree.addEventListener('click', handleDisplayFaqThree)
 faqFour.addEventListener('click', handleDisplayFaqFour)
 
+// FAQ Logic (end)
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+// Form Logic (start)
 
 const errorContainer = document.querySelector('.input-container');
 const input: any = document.querySelector('.input-container input');
-
-console.log(document.hasFocus())
+const form = document.forms[0];
 
 const handleSubmitForm = (e) => {
     e.preventDefault();
@@ -188,6 +226,9 @@ const handleSubmitForm = (e) => {
     }
 }
 
-const form = document.forms[0];
-
 form.addEventListener('submit', handleSubmitForm);
+
+// Form Logic (end)
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------
